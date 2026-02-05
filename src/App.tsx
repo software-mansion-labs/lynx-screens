214a8ae16e5ef7d53e91ff90d296b36ae3b1f487
Tomasz Boroń <tomasz.boron@swmansion.com>
2026-02-05 10:24:54 +0100

    Update App

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ export function App(props: { onRender?: () => void }) {
         style={{
           flex: 1,
           borderColor: '#f01313',
-          borderWidth: 2,
+          borderWidth: '2px',
           display: 'flex',
         }}
         backgroundColorHex={color}
@@ -21,8 +21,8 @@ export function App(props: { onRender?: () => void }) {
           style={{
             backgroundColor: '#f01313',
             display: 'flex',
-            height: 100,
-            width: 100,
+            height: '100px',
+            width: '100px',
           }}
         />
       </color-box-view>
@@ -30,7 +30,7 @@ export function App(props: { onRender?: () => void }) {
         style={{
           flex: 1,
           borderColor: '#f01313',
-          borderWidth: 2,
+          borderWidth: '2px',
           display: 'flex',
         }}
         backgroundColorHex={color}
@@ -39,8 +39,8 @@ export function App(props: { onRender?: () => void }) {
           style={{
             backgroundColor: '#f01313',
             display: 'flex',
-            height: 100,
-            width: 100,
+            height: '100px',
+            width: '100px',
             justifyContent: 'flex-end',
             alignItems: 'flex-end',
           }}
@@ -49,8 +49,8 @@ export function App(props: { onRender?: () => void }) {
             style={{
               backgroundColor: '#1364f0',
               display: 'flex',
-              height: 50,
-              width: 50,
+              height: '50px',
+              width: '50px',
             }}
           />
         </view>
