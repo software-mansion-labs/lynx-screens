04a6716c301b38bf779e16054a5d127dad1fef31
Tomasz Boroń <tomasz.boron@swmansion.com>
2026-02-09 14:22:30 +0100

    Trigger re-layout on push on the Host level

--- android/app/src/main/java/com/lynxscreens/screens/host/StackHostComponent.kt
+++ android/app/src/main/java/com/lynxscreens/screens/host/StackHostComponent.kt
@@ -25,8 +25,20 @@ internal class StackHostComponent(context: LynxContext) : UIGroup<StackHostView>
     override fun insertChild(child: LynxBaseUI, index: Int) {
         require(child is StackScreenComponent) { "[RNScreens] Attempt to attach child that is not of type ${StackScreenComponent::javaClass.name}" }
 
-        mountLynxSubviewAt(child, index)
-        super.insertChild(child, index)
+        // Store the reference to StackHost, to notify it when the activity mode will change from DETACHED to ATTACHED
+        child.stackHost = WeakReference(this)
+
+        if (child.activityMode == StackScreenComponent.ActivityMode.ATTACHED) {
+            // Insert always at the last index
+            mountLynxSubviewAt(child)
+            // We manually trigger a layout pass because we have disabled Lynx's default
+            // native view management for StackHostComponent. Since the FragmentManager
+            // now handles the insertion of views into this container, we must ensure
+            // that the CoordinatorLayout (StackContainer) re-measures its children when relevant.
+            requestLayout()
+            // Add the component to Lynx children, only when it's attached
+            super.insertChild(child, super.getChildCount())
+        }
     }
 
     override fun insertView(child: LynxUI<*>?) {
@@ -66,11 +78,9 @@ internal class StackHostComponent(context: LynxContext) : UIGroup<StackHostView>
     }
 
     private fun mountLynxSubviewAt(
-        stackScreen: StackScreenComponent,
-        index: Int,
+        stackScreen: StackScreenComponent
     ) {
-        renderedScreens.add(index, stackScreen)
-        stackScreen.stackHost = WeakReference(this)
+        renderedScreens.add(stackScreen)
         addPushOperationIfNeeded(stackScreen)
     }
 
@@ -101,9 +111,21 @@ internal class StackHostComponent(context: LynxContext) : UIGroup<StackHostView>
 
     internal fun stackScreenChangedActivityMode(stackScreen: StackScreenComponent) {
         when (stackScreen.activityMode) {
-            StackScreenComponent.ActivityMode.DETACHED -> containerUpdateCoordinator.addPopOperation(stackScreen)
-            StackScreenComponent.ActivityMode.ATTACHED -> containerUpdateCoordinator.addPushOperation(stackScreen)
+            StackScreenComponent.ActivityMode.DETACHED -> onStackScreenChangedActivityModeFromAttachedToDetached(stackScreen)
+            StackScreenComponent.ActivityMode.ATTACHED -> onStackScreenChangedActivityModeFromDetachedToAttached(stackScreen)
         }
+    }
+
+    private fun onStackScreenChangedActivityModeFromAttachedToDetached(stackScreen: StackScreenComponent) {
+        containerUpdateCoordinator.addPopOperation(stackScreen)
+    }
+
+    private fun onStackScreenChangedActivityModeFromDetachedToAttached(stackScreen: StackScreenComponent) {
+        // By default, Lynx attempts to attach children as soon as an insert operation happens.
+        // To handle preloading, we need manual control over child attachment.
+        // Therefore, this adjustment is to call the whole insertChild logic which we skipped earlier.
+        // It should be only executed when we have confirmed the state is **changed** to ATTACHED.
+        insertChild(stackScreen, super.getChildCount())
     }
 
     override fun onScreenDismiss(stackScreen: StackScreenComponent) = Unit
