44e48750033a49dcb9a297f9b1ee439fda5f8105
Tomasz Boroń <tomasz.boron@swmansion.com>
2026-03-05 10:04:25 +0100

    Prevent mounting Screen as a direct child of Host

--- apple/LynxScreens/screens/host/RNSStackHostView.mm
+++ apple/LynxScreens/screens/host/RNSStackHostView.mm
@@ -1,5 +1,6 @@
 #import "RNSStackHostView.h"
 #import "RNSStackHostComponent.h"
+#import "RNSStackScreenView.h"
 
 @implementation RNSStackHostView
 
@@ -9,6 +10,21 @@
     if (self.window) {
         [self.component viewDidMoveToWindow];
     }
+}
+
+- (void)insertSubview:(UIView *)view atIndex:(NSInteger)index
+{
+    /**
+     * `LynxUI.insertChild` method automatically calls `insertSubview`, taking the management over the native hierarchy.
+     * However, we need to intercept and ignore this call when dealing with a `StackHost` - `StackScreen` communication.
+     * While attaching a `StackScreen` as a Lynx child of a `StackHost` is reasonable for Lynx component hierarchy (similarly to `reactSubviews`),
+     * the native hierarchy requires screen management to be realized by `RNStackController` (at the view controller level),
+     * rather than relying on direct insertions.
+     */
+    if ([view isKindOfClass:RNSStackScreenView.class]) {
+        return;
+    }
+    [super insertSubview:view atIndex:index];
 }
 
 @end
